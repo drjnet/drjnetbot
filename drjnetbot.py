06d3bec3675b806ed10a420c9648345b59fbaa0d
dave jenkins <david@logicc.co.uk>
2019-07-05 11:49:11 +0100

    moving price grab to function

--- drjnetbot.py
+++ drjnetbot.py
@@ -1,41 +1,32 @@
-import os, requests, json, pprint
-import telebot,unicodedata
+#!/usr/bin/python
 
+import os, requests, json, pprint, telebot, logging, traceback
+import config
 
 # Grab BTC Prices from blockchain.info
-r = requests.get("https://blockchain.info/ticker") 
-json_data = json.loads(r.text)
+# r = requests.get("https://blockchain.info/ticker") 
+# json_data = json.loads(r.text)
+   
+# Store prices in strings for output
+# price_gbp = "GBP" + str((json_data['GBP']['last']))
+# price_euro = "EUR" + str((json_data['EUR']['last']))
+# price_output="Latest BTC Price \n------------------\n" + price_gbp + " / " + price_euro
+# print (price_output)
+
 
 def get_prices(currency):
-    float price
+    price = 0
     r = requests.get("https://blockchain.info/ticker") 
     json_data = json.loads(r.text)
     price=int(round((json_data[currency]['last'])))
-    print price
     return price
-    
-print "Prices are " + price
 
-# Store prices in strings for output
-price_gbp=int(round((json_data['GBP']['last'])))
-price_eur=int(round((json_data['EUR']['last'])))
-print str(price_gbp) + str(price_eur)
+priceTicker = "EUR " + str(get_prices("EUR")) + " / " + "GBP " + str(get_prices("GBP"))
 
-price_gbp="GBP" + str((json_data['GBP']['last']))
-price_euro="EUR" + str((json_data['EUR']['last']))
-price_output="Latest BTC Price \n------------------\n" + price_gbp + " / " + price_euro
-print price_output
+print priceTicker
 
-# def show_prices ():
-#     print "Latest BTC Price"
-#     print "------------------"
-#     print price_gbp + " / " + price_euro
-
-# show_prices()
-
-token = "860537932:AAGsD5o2ikSCoJzbmghlFovWBEy0MghOkE8"
+token = "{telegramtoken}"
 bot = telebot.TeleBot(token)
-
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -43,9 +34,6 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['price'])
 def send_welcome(message):
-    bot.reply_to(message, price_output)
+    bot.reply_to(message, priceTicker)
 
 bot.polling()
-
-
-
